cf4573cf6832911d78a8bc2395513f9e112baef7
Ольга Лосева <olgalo@list.ru>
2024-02-03 14:11:50 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Angle.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Angle
+    {
+        int gradus;   
+        int min;
+        int sec;
+
+        public int Gradus    
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                gradus = value % 360;
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                Gradus += value / 60;
+                min = value % 60;
+            }
+        }
+
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                Min += value / 60;
+                sec = value % 60;
+            }
+        }
+        public Angle(int gradus, int min, int sec)      //конструктор
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        //Метод перевода угла в радианы
+        public double ToRadians()           
+        {
+            double angleDec = ((double)(sec + min * 60 + gradus * 60 * 60))/3600;
+            return angleDec*Math.PI/180;
+        }
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        //Угол задан целочисленными значениями градусов, угловых минут и угловых секунд.
+        //Реализовать класс, в котором указанные значения представлены в виде свойств. 
+        //Предусмотреть в свойствах проверку корректности данных.
+        //Класс должен содержать конструктор и метод ToRadians для перевода угла в радианы.
+        //В программе создать объект указанного класса и использовать его.
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ПЕРЕВОД УГЛА ИЗ ГРАДУСОВ В РАДИАНЫ");
+            Console.WriteLine("___________________________________");
+            Console.WriteLine();
+
+            int gradusRead, minRead, secRead;
+
+            do
+            {
+                Console.Write("Задайте угол ложительными числами: градусы - ");
+                gradusRead = Convert.ToInt32(Console.ReadLine());
+                Console.Write("угловые минуты - ");
+                minRead = Convert.ToInt32(Console.ReadLine());
+                Console.Write("угловые секунды - ");
+                secRead = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+            } while (gradusRead<0|| minRead<0|| secRead<0);
+
+            Angle ang = new Angle(gradusRead, minRead, secRead);
+
+            double radians = ang.ToRadians();
+            Console.WriteLine("Угол в радианах составит: {0}", radians);
+            Console.ReadKey();
+        }
+    }
+}
